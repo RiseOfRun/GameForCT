af9f5f4fb6de8804dd05e0ad86124ac5e8d67071
Runi <runi54rus@gmail.com>
2020-11-12 19:29:46 +0700

    Added class Cat.cs

---
+++ SomeProject/EasyProgram/EasyProgram/Cat.cs
@@ -0,0 +1,14 @@
+﻿namespace EasyProgram
+{
+    class Cat
+    {
+        public int speed = 5;
+        public int position = 0;
+
+        public int Walk()
+        {
+            position += speed;
+            return position;
+        }
+    }
+}
